fcd5b731aca7dfcede5eedfdc5832ed69895ca2f
Daniel Tavares <toys.tokio@gmail.com>
2026-09-12 00:22:09 -0300

    adiciona leitura das instruções MIPS

--- src/simulador_mips.py
+++ src/simulador_mips.py
@@ -3,4 +3,7 @@ import json
 with open("entrada/entrada.json", "r") as arquivo:
     dados = json.load(arquivo)
 
-    print(dados)
+    instrucoes = dados["text"]
+
+    for instrucao in instrucoes:
+        print(instrucao)
